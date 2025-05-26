1ba276d57578670200669f4294bc07416664c4d1
Antônio Della Flora <antoniodellaflora@gmail.com>
2025-05-26 08:57:42 -0300

    Adiciona script para conversão de modelo de rede neural treinado para formato C utilizando a biblioteca emlearn.

---
+++ stm32_rede_neural/Core/converte_emlearn.py
@@ -0,0 +1,9 @@
+import joblib
+import emlearn
+
+# Carregar o modelo treinado
+modelo = joblib.load('modelo_rede_neural.joblib')
+
+# Converter para C
+cmodel = emlearn.convert(modelo)
+cmodel.save(file='rede_neural.h')
